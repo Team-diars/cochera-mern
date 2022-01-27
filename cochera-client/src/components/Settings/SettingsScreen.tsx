a2e1ac0a4dd7fdc934ce9079e8642994047cbe5a
Hector Herrera <hectorsum81@hotmail.com>
2022-01-26 22:31:45 -0500

    update: setting up settings screen

--- cochera-client/src/components/Settings/SettingsScreen.tsx
+++ cochera-client/src/components/Settings/SettingsScreen.tsx
@@ -1,9 +1,52 @@
+import { Button } from '@chakra-ui/button'
+import { FormControl, FormLabel } from '@chakra-ui/form-control'
+import { Input, InputGroup, InputLeftAddon, InputLeftElement } from '@chakra-ui/input'
+import { Box, Container, Text } from '@chakra-ui/layout'
 import React from 'react'
+import { FiPhoneIncoming } from 'react-icons/fi'
 
 export const SettingsScreen = () => {
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <Container paddingY="10" >
+      <Text
+        fontSize="2xl"
+        fontWeight="bold"
+        lineHeight="short"
+        marginBottom="6"
+      >
+        Configuracion de Cochera
+      </Text>
+      <Box p="4" borderWidth="1px" borderRadius="md" bg="#fff">
+        <FormControl marginBottom="3">
+            <FormLabel>Precio Tarifa</FormLabel>
+            <InputGroup>
+            <InputLeftElement
+              pointerEvents='none'
+              color='gray.300'
+              fontSize='1.2em'
+              children={"S/."}
+            />
+              <Input
+                type="text"
+                name="appname"
+                placeholder="Ingresar Precio"
+              />
+            </InputGroup>
+        </FormControl>
+        <Box
+          mt="4"
+          display="flex"
+          justifyContent="flex-end"
+          alignItems="center"
+        >
+          <Button
+            colorScheme="blue"
+            type="submit"
+          >
+            Guardar
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   )
 }
--- cochera-client/src/state/reducers/index.ts
+++ cochera-client/src/state/reducers/index.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 import customerReducer from './CustomerReducer';
 import carReducer from './CarReducer';
+import settingsReducer from './SettingsReducer';
 const reducers = combineReducers({
   customers: customerReducer,
-  cars: carReducer
+  cars: carReducer,
+  settings: settingsReducer
 })
 
 export default reducers;
